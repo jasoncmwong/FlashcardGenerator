edcee315fe42f73fe66eed128ca4a33423f1b188
jcwong2 <jcwong2@ualberta.ca>
2020-01-11 22:23:35 -0700

    Removed flipped flashcard layout. Modified TextView's in the layout instead.

--- app/src/main/java/com/example/flashcardgenerator/TestFlashcard.java
+++ app/src/main/java/com/example/flashcardgenerator/TestFlashcard.java
@@ -2,9 +2,11 @@ package com.example.flashcardgenerator;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.view.ViewGroup;
 import android.widget.TextView;
 
 public class TestFlashcard extends AppCompatActivity {
@@ -15,21 +17,21 @@ public class TestFlashcard extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_test_flashcard);
-        TextView debugView = findViewById(R.id.debug_default);
-        debugView.setText(Boolean.toString(cardFlipped));
     }
 
     public void flipCard(View v) {
-        if (!cardFlipped) {
-            setContentView(R.layout.flipped_flashcard);
-            cardFlipped = true;
-            TextView debugView = findViewById(R.id.debug_flip);
-            debugView.setText(Boolean.toString(cardFlipped));
-        } else {
-            setContentView(R.layout.activity_test_flashcard);
+        if (cardFlipped) {
             cardFlipped = false;
-            TextView debugView = findViewById(R.id.debug_default);
-            debugView.setText(Boolean.toString(cardFlipped));
+            TextView topRow = findViewById(R.id.top_row);
+            TextView botRow = findViewById(R.id.bot_row);
+            topRow.setText(getResources().getString(R.string.py_flashcards));
+            botRow.setText(getResources().getString(R.string.cc_flashcards));
+        } else {
+            cardFlipped = true;
+            TextView topRow = findViewById(R.id.top_row);
+            TextView botRow = findViewById(R.id.bot_row);
+            topRow.setText(getResources().getString(R.string.def_flashcards));
+            botRow.setText("");
         }
     }
 }
