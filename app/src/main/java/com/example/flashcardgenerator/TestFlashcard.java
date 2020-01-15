3d91b11eaab0b304918e173ee16fc60689412c5a
jcwong2 <jcwong2@ualberta.ca>
2020-01-14 21:43:27 -0700

    Generalized gesture event handling, card display, and indexing to match .csv file. Added displayInfo function to handle displaying info given the card index and flip state.

--- app/src/main/java/com/example/flashcardgenerator/TestFlashcard.java
+++ app/src/main/java/com/example/flashcardgenerator/TestFlashcard.java
@@ -13,54 +13,74 @@ import java.util.ArrayList;
 
 public class TestFlashcard extends AppCompatActivity {
 
-    public static boolean cardFlipped = false;
+    boolean cardFlipped = false;
     ArrayList<String[]> cardData = new ArrayList<String[]>();
+    int cardIndex = 1;
+    TextView topRow;
+    TextView botRow;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_test_flashcard);
-        View flashcard = findViewById(R.id.flashcard);
+        final View flashcard = findViewById(R.id.flashcard);
+        topRow = findViewById(R.id.top_row);
+        botRow = findViewById(R.id.bot_row);
+
         flashcard.setOnTouchListener(new OnSwipeTouchListener() {
             @Override
             public void onSwipeLeft() {
-                TextView topRow = findViewById(R.id.top_row);
-                TextView botRow = findViewById(R.id.bot_row);
-                topRow.setText(cardData.get(2)[0]);
-                botRow.setText(cardData.get(2)[1]);
+                cardIndex += 1;
+                if (cardIndex < cardData.size()) {
+                    displayInfo(cardIndex, false);
+                } else if (cardIndex == cardData.size()) {
+                    topRow.setText("Reached end of list");
+                    botRow.setText("");
+                    topRow.setTextSize(40);
+                } else {
+                    cardIndex = 1;
+                    displayInfo(cardIndex, false);
+                }
             }
 
             @Override
             public void onSwipeRight() {
-                TextView topRow = findViewById(R.id.top_row);
-                TextView botRow = findViewById(R.id.bot_row);
-                topRow.setText(cardData.get(0)[0]);
-                botRow.setText(cardData.get(0)[1]);
+                cardIndex -= 1;
+                if (cardIndex > 0) {
+                    displayInfo(cardIndex, false);
+                } else if (cardIndex == 0) {
+                    topRow.setText("Reached start of list");
+                    botRow.setText("");
+                    topRow.setTextSize(40);
+                } else {
+                    cardIndex = cardData.size() - 1;
+                    displayInfo(cardIndex, false);
+                }
             }
         });
         Intent intent = getIntent();
         cardData = (ArrayList<String[]>) intent.getSerializableExtra("FLASHCARD_DATA");
-        TextView topRow = findViewById(R.id.top_row);
-        TextView botRow = findViewById(R.id.bot_row);
-        topRow.setText(cardData.get(1)[0]);
-        botRow.setText(cardData.get(1)[1]);
+        displayInfo(cardIndex, false);
+        cardFlipped = false;
     }
 
     public void flipCard(View v) {
-        if (cardFlipped) {
-            cardFlipped = false;
-            TextView topRow = findViewById(R.id.top_row);
-            TextView botRow = findViewById(R.id.bot_row);
-            topRow.setText(cardData.get(1)[0]);
-            topRow.setTextSize(40);
-            botRow.setText(cardData.get(1)[1]);
-        } else {
-            cardFlipped = true;
-            TextView topRow = findViewById(R.id.top_row);
-            TextView botRow = findViewById(R.id.bot_row);
-            topRow.setText(cardData.get(1)[2]);
-            topRow.setTextSize(20);
+        if (cardIndex > 0 && cardIndex < cardData.size()) {  // Data to display when flipped
+            cardFlipped = !cardFlipped;
+            displayInfo(cardIndex, cardFlipped);
+        }
+    }
+
+    private void displayInfo(int index, boolean flipState) {
+        cardFlipped = flipState;
+        if (flipState) {  // Info for card flipped (answer)
+            topRow.setText(cardData.get(cardIndex)[2]);
             botRow.setText("");
+            topRow.setTextSize(30);
+        } else {  // Info for card not flipped (question)
+            topRow.setText(cardData.get(index)[0]);
+            botRow.setText(cardData.get(index)[1]);
+            topRow.setTextSize(40);
         }
     }
 }
